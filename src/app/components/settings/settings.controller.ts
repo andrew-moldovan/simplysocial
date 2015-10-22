5ec9f22e792eb3ec2be76cbd2eea29ca26a6b587
Andrew Moldovan <anmoldov@microsoft.com>
2015-10-22 08:24:54 -0700

    added in all the settings to the settings page, can now properly edit them

--- src/app/components/settings/settings.controller.ts
+++ src/app/components/settings/settings.controller.ts
@@ -2,9 +2,16 @@ module simplysocial {
   'use strict';
 
   export class SettingsController {
+    public user: any;
+
     /* @ngInject */
-    constructor() {
-      console.log("inside settings controller");
+    constructor(private userService: UserService) {
+      this.user = userService.getLoggedInUser();
+    }
+
+    public saveSettings() {
+      // Make the necessary $http call to save the settings
+      console.log(this.user);
     }
   }
 
--- src/app/components/user/user.service.ts
+++ src/app/components/user/user.service.ts
@@ -11,7 +11,48 @@ module simplysocial {
       this.loggedInUser = {
         displayName: "Andrew Moldovan",
         firstName: "Andrew",
-        lastName: "Moldovan"
+        lastName: "Moldovan",
+        password: "some-hashed-value",
+        email: "andrew@andrewmoldovan.com",
+        notificationSettings: [
+          { text: "Email me when my posts are marked as favorites", value: true },
+          { text: "Email me when I'm mentioned", value: true },
+          { text: "Email me when I get a reply", value: true },
+          { text: "Email me when someone follows me", value: true }
+        ],
+        privacySettings: [
+          {
+            grouping: "tags",
+            selected: 'anyone',
+            values: [
+              { text: "allow anyone to tag me", value: 'anyone' },
+              { text: "only allow people I follow to tag me", value: 'follow' },
+              { text: "don't allow anyone to tag me", value: 'noone' },
+            ]
+          },
+          {
+            grouping: "location",
+            selected: 'add',
+            values: [
+              { text: "add a location to my posts", value: 'add' }
+            ]
+          },
+          {
+            grouping: "email",
+            selected: 'find',
+            values: [
+              { text: "let others find me by my email address", value: 'find' }
+            ]
+          },
+          {
+            grouping: "ads",
+            selected: 'tailor',
+            values: [
+              { text: "tailor ads based on my information", value: 'tailor' }
+            ]
+          }
+          
+        ]
       }
     }
 
