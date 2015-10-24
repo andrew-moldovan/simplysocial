88c82e14fcd1151f8b695da309d2c00b6d731c2a
Andrew Moldovan <anmoldov@microsoft.com>
2015-10-23 21:36:52 -0700

    fixed the navbar to the top

--- src/app/components/post/post.service.ts
+++ src/app/components/post/post.service.ts
@@ -10,7 +10,7 @@ module simplysocial {
         { displayName: "Sam Soffes", 
           timestamp: 1445441030000,
           message: "How To Get Inspired: the Right Way - Designmodo <a href='http://bit.ly/1lE4uJc'>bit.ly/1lE4uJc</a> Good stuff from <span class='at-mention'>@designmodo</span>!",
-          imageUrl: 'blah',
+          imageUrl: '../assets/images/pic1.png',
           replies: [
             { displayName: "Jed Bridges", timestamp: 1445443030000, message: "Great way to start the week. Thanks for sharing" },
             { displayName: "Ren Walker", timestamp: 1445445030000, message: "Feeling inspired now... thanks for the great article <span class='at-mention'>@designmodo!</span>" },
@@ -19,7 +19,8 @@ module simplysocial {
         { displayName: "Meg Robichaud", 
           timestamp: 1444771738424, 
           message: "My view this morning is simply beautiful",
-          replies: []
+          replies: [],
+          imageUrl: '../assets/images/pic2.png'
         }
       ];
     }
